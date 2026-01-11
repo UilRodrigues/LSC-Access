df4405c5286e62355379081005f88e957331d92f
¨Uilson¨ <rodriguesuilson94@gmail.com>
2026-01-11 14:58:19 -0500

    first commit base source

---
+++ src/lsc_dictionary.py
@@ -0,0 +1,14 @@
+LSC_DICTIONARY = {
+    "viajero": "VIAJERO",
+    "familia": "FAMÍLIA",
+    "montaña": "MONTAÑA",
+    "mar": "MAR",
+    "descansar": "DESCANSAR",
+    "fin": "FIN-DE-SEMANA",
+    "semana": "SEMANA",
+    "hoy": "HOY",
+    "ayer": "AYER",
+    "mañana": "MAÑANA",
+    "ir": "IR",
+    "fue": "FUE"
+}
